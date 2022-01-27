559edf01b5f2f39bda2b0abb1e4a73a27c8e7645
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2022-01-27 09:47:21 -0700

    add servlets and jsp
    add txt jsp and servlets

---
+++ Week4Lab_SimpleNoteKeeper/src/main/java/crud/ca/sait/week4lab_simplenotekeeper/javabeans/Note.java
@@ -0,0 +1,11 @@
+package crud.ca.sait.week4lab_simplenotekeeper.javabeans;
+
+import java.io.Serializable;
+
+/**
+ * Represetns a Note
+ * @author Kevin Bai, 000825241
+ */
+public class Note implements Serializable {
+    
+}
