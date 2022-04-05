8c3560f173ffca872796c31673a411278771f854
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2022-04-04 23:11:32 -0600

    update adminfilter
    add functionality for filtering logins

--- lab8/SecurityDemo/src/java/filters/AdministrationFilter.java
+++ lab8/SecurityDemo/src/java/filters/AdministrationFilter.java
@@ -14,29 +14,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class AdministrationFilter implements Filter {
+public class AuthenticationFilter implements Filter {
     
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain)
             throws IOException, ServletException {
-
-            // code that is executed before the servlet
-            HttpServletRequest httpRequest = (HttpServletRequest)request;
-            HttpSession session = httpRequest.getSession();
-            String email = (String)session.getAttribute("email");
-            
-            if (email == null) {
-                HttpServletResponse httpResponse = (HttpServletResponse)response;
-                httpResponse.sendRedirect("login");
-                return;
-            }
-            
-            chain.doFilter(request, response); // execute the servlet
-            
-            // code that is executed after the servlet
-            
-        
+    	
+    	HttpServletRequest validationRequest = (HttpServletRequest) request;
+    	HttpServletResponse validationResponse = (HttpServletResponse) response;
+    	HttpSession session = validationRequest.getSession();
+    	
+    	//initializes to 0
+    	int userRole = 0;
+    	userRole = (int)session.getAttribute("roleId");
+    	
+    	//initializes to null
+    	String userEmail = null;
+    	userEmail = (String)session.getAttribute("email");
+    	
+    	//role and email contain valid values
+    	if(userEmail != null && !userEmail.isEmpty() && !userEmail.isBlank() && userRole != 0) {
+    		//is administrator
+    		if(userRole == 1) {
+    			chain.doFilter(request,response);
+    		//valid but isn't administrator
+    		} else {
+    			validationResponse.sendRedirect("/notes");
+    		}
+    	} else {
+    		validationResponse.sendRedirect("/login");
+    	}
+    	
+   
+    	
     }
 
     @Override
