ba0603e982a8ab237df52c1a6a4c1c3be296501d
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2022-04-09 06:09:13 -0600

    fixed lab 8

---
+++ lab8/build/generated-sources/ap-source-output/models/Role_.java
@@ -0,0 +1,17 @@
+package models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import models.User;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-04-09T06:07:11")
+@StaticMetamodel(Role.class)
+public class Role_ { 
+
+    public static volatile ListAttribute<Role, User> userList;
+    public static volatile SingularAttribute<Role, Integer> roleId;
+    public static volatile SingularAttribute<Role, String> roleName;
+
+}
